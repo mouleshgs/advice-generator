ce933d00fc317cd8920bd4dc6a2c80848395d6c5
mouleshleo <mouleshleo06@gmail.com>
2025-03-05 22:43:07 +0530

    main changed

--- src/advice.jsx
+++ src/advice.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 
 function Advice({ fetchNewAdvice, advice, loading, error }) {
+    
     return (
         <div className="flex flex-col items-center gap-5 p-5 pt-1 text-gray-600">
             {loading ? <p className="text-xl">Loading...</p> : <p className="text-2xl opacity-90">{error || advice}</p>}
         </div>
     );
+
 }
 
 export default Advice;
